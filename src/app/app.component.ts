fd4925aadeb03e28c9c296a27aed5f15f0d62094
Priti Shreya <priti@promact.com>
2018-02-07 20:20:57 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '<employee>',
-  templateUrl: './employee.html',
-  styleUrls: ['./app.component.css']
+  selector: 'employee',
+  templateUrl:'./app.component.html'
 }) 
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { EmployeeComponent } from './employee.component';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
+import { ListComponent } from './list.component';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-   // AppComponent,
-    EmployeeComponent
-    
+    AppComponent,
+    EmployeeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+     RouterModule.forRoot([
+     {path:'main',component:EmployeeComponent},
+     {path:'list',component:ListComponent},
+     ])
   ],
   providers: [],
-  bootstrap: [EmployeeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
